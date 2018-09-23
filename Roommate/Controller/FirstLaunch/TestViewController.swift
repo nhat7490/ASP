5a45045d313c7be41e3d5c79c2590838d347db11
nhat7490 <nhat_nguyen7490@yahoo.com>
2018-09-23 20:11:24 +1400

    trial

--- Roommate/Controller/FirstLaunch/TestViewController.swift
+++ Roommate/Controller/FirstLaunch/TestViewController.swift
@@ -14,7 +14,9 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view. test
+        // Do any additional setup after loading the view. test 
+		//additional
+		//after
     }
 
     override func didReceiveMemoryWarning() {
